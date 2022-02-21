21c71d88479d827a9215d727ca4dbf8d38400006
Alex <ahover307@gmail.com>
2022-02-21 14:30:49 -0500

    Order by amt guess

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,14 +1,16 @@
-import {addDoc, collection, getDocs, getFirestore, query, where} from "firebase/firestore/lite";
+import {addDoc, collection, getDocs, getFirestore, orderBy, query, where} from "firebase/firestore/lite";
 import {firebaseApp} from './config'
 
 const db = getFirestore(firebaseApp);
 const path = 'daily_results'
 
+//Runs query for all results on a supplied wordle.
+//Order bys amount of guesses
 async function queryByResults(wordle) {
-    return (await getDocs(query(collection(db, path), where('wordle', '==', wordle))));
+    return (await getDocs(query(collection(db, path), where('wordle', '==', wordle), orderBy('guess'))));
 }
 
-
+//Upload object into firestore and generates the time it was uploaded
 async function uploadResults(user, result, message, wordle, guess) {
     return (await addDoc(collection(db, path), {
         user: user,
